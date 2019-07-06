9e07c5be9232443c2ba0def13b923bd9702f91e2
lucagez <lucagesmundo@yahoo.it>
2019-07-06 13:23:27 +0200

    adding scratches

---
+++ scratch/react/index.js
@@ -0,0 +1,62 @@
+
+// File made for developing purpose
+
+const { Component, useState, useEffect } = React;
+const { create, use, get } = msms;
+
+create('COUNTER', {
+  culo: {
+    default: 0,
+    action: (state, event) => {
+      return state.culo + event
+    },
+  },
+  count: {
+    default: 0,
+    validate: (count) => count > -1,
+    action: (state, event) => {
+      state.send('culo', 45);
+      return state.count + event
+    },
+  },
+});
+
+const C1 = ({ id }) => {
+  const [send, state, on] = use('COUNTER');
+  const [, trigger] = useState();
+
+  on(trigger);
+
+  return (
+    <div>
+      {state.count}
+      {[1, -1].map((event, i) => (
+        <button
+          key={i}
+          type="button"
+          onClick={() => send('count', event)}
+        >
+          {event === 1 ? '+' : '-'}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.count > 5 && <h1>HI! (:</h1>}
+        {/* {Array(10).fill(0).map((_, i) => <C1 id={i} />)} */}
+        <C1 />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
